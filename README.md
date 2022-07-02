b92b72c515db45f682ec0d3f76d2214ae6bd9ea4
ForsakenRei <23041178+ForsakenRei@users.noreply.github.com>
2022-07-02 19:37:10 -0400

    update v3 keymap, add flash guide to readme

--- README.md
+++ README.md
@@ -1,3 +1,16 @@
 # qmk_kbd67lite
 
-QMK firmware archive for KBD67Lite R2
+QMK firmware archive for KBD67Lite R2 and R3
+
+#### V2 PCB Flash Guide
+
+1. Hold `ESC` and plug the board in
+2. Clear EEPROM in QMK Toolbox
+3. Flash the firmware
+
+#### V3 PCB Flash Guide
+
+1. Hold `ESC` and plug the board in
+2. Open the USB flash drive and delete `FLASH.bin`
+3. Paste the new firmware `.bin` to the flash drive
+4. Eject the board first then unplug the board
--- kbdfans/kbd67/mkiirgb/keymaps/shigure_gaming/keymap.c
+++ kbdfans/kbd67/mkiirgb/keymaps/shigure_gaming/keymap.c
@@ -15,8 +15,14 @@
 
 #include QMK_KEYBOARD_H
 
-void matrix_init_user(void) {
-  rgb_matrix_sethsv(0, 0, 0); // set default RGB color
+void keyboard_post_init_user(void) {
+  #ifdef RGB_MATRIX_ENABLE
+       rgb_matrix_sethsv(0, 0, 0); // set default RGB color
+  #endif 
+  if (IS_HOST_LED_ON(USB_LED_NUM_LOCK))
+  { // turn on Num lock by defautl
+    tap_code(KC_NUMLOCK);
+  }
 };
 
 enum custom_keycodes
@@ -118,9 +124,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                       _______, _______,  _______, _______, _______, _______, _______, DC_MUTE, _______, _______, _______,            _______,  RGB_MOD,   RGB_HUD,
                                       _______, _______,  _______,                   _______,                   TO(0),            TO(4),              RGB_VAD,  RGB_RMOD,  RGB_VAI),
 	  [_L4] = LAYOUT_65_ansi_blocker( /* Gaming */
-                                      KC_GESC, KC_1,     KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,     KC_MINS,  KC_EQL,   KC_BSPC,   DC_MUTE,
-                                      KC_TAB,  KC_Q,     KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,     KC_LBRC,  KC_RBRC,  KC_BSLASH, KC_PGUP,
-                                      KC_CAPS, KC_A,     KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN,  KC_QUOT,            KC_ENT,    KC_PGDN,
+                                      KC_GESC, KC_1,     KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,     KC_MINS,  KC_EQL,   KC_BSPC,   KC_MUTE,
+                                      KC_TAB,  KC_Q,     KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,     KC_LBRC,  KC_RBRC,  KC_BSLASH, KC_F13,
+                                      KC_CAPS, KC_A,     KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN,  KC_QUOT,            KC_ENT,    KC_F14,
                                       KC_LSFT, KC_Z,     KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,            KC_RSFT,  KC_UP,     KC_LGUI,
                                       KC_LCTL, KC_NO,    KC_LALT,                   KC_SPC,                    TO(0),            KC_RCTL,            KC_LEFT,  KC_DOWN,   KC_RGHT)                                     
 }; 
@@ -311,14 +317,6 @@ qk_tap_dance_action_t tap_dance_actions[] = {
     [KCFN_L2] = ACTION_TAP_DANCE_FN_ADVANCED(NULL, fn_finished, fn_reset),
     [RSFT_LAY3] = ACTION_TAP_DANCE_FN_ADVANCED(NULL, sft_finished, sft_reset)
 };
-
-void keyboard_post_init_user(void)
-{
-  if (IS_HOST_LED_ON(USB_LED_NUM_LOCK))
-  { // turn on Num lock by defautl
-    tap_code(KC_NUMLOCK);
-  }
-}
 
 const key_override_t delete_key_override = ko_make_basic(MOD_MASK_SHIFT, KC_BSPACE, KC_DELETE);
 
