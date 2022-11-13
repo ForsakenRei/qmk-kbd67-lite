0aff63fbdaab9afb24dafd2e0ba8abf365f8c1e7
ForsakenRei <23041178+ForsakenRei@users.noreply.github.com>
2022-11-13 13:50:42 -0500

    misc update

--- kbdfans/kbd67/mkiirgb/keymaps/shigure/keymap.c
+++ kbdfans/kbd67/mkiirgb/keymaps/shigure/keymap.c
@@ -1,4 +1,4 @@
-/* Copyright 2022 shigure
+/* Copyright 2022 Shigure
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 2 of the License, or
